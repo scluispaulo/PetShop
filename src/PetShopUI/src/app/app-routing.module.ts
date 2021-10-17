ad29329293611d789f4f152acaa2f54edf6968a8
Paulo Costa <scluispaulo@gmail.com>
2021-10-17 16:46:37 -0300

    Create base components

--- src/PetShopUI/src/app/app-routing.module.ts
+++ src/PetShopUI/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccommodationsComponent } from './components/accommodations/accommodations.component';
+import { EditPetComponent } from './components/edit-pet/edit-pet.component';
+import { IndexComponent } from './components/index/index.component';
+import { NewPetComponent } from './components/new-pet/new-pet.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: IndexComponent },
+  { path: 'newpet', component: NewPetComponent },
+  { path: 'editpet', component: EditPetComponent },
+  { path: 'accommodations', component: AccommodationsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/PetShopUI/src/app/app.module.ts
+++ src/PetShopUI/src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { EditPetComponent } from './components/edit-pet/edit-pet.component';
+import { NewPetComponent } from './components/new-pet/new-pet.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AccommodationsComponent } from './components/accommodations/accommodations.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    FooterComponent,
+    HeaderComponent,
+    EditPetComponent,
+    NewPetComponent,
+    AccommodationsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
